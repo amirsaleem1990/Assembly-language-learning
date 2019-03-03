514a3d98f30fabbb4dee9743117ed53e3ed4a48c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-03-03 16:53:01 +0500

    new

---
+++ multiplication-of-tow-numbers.asm
@@ -0,0 +1,28 @@
+; program to multiply two numbers
+.model small
+.stack 100h
+.data
+.code
+main proc
+mov al, 5
+mov bl, 2
+mul bl
+AAM
+mov ch, ah
+mov cl, al
+
+mov dl, ch
+add dl, 48
+mov ah, 2
+int 21h
+
+mov dl, cl
+add dl, 48
+mov ah, 2
+int 21h
+
+mov ah, 4ch
+int 21h
+
+main endp
+end main
